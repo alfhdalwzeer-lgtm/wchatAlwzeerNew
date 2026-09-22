0f20f6ed463ecd610b4ec323a61267d31653d9ff
alfhdalwzeer-lgtm <alfhdalwzeer@gmail.com>
2026-09-22 19:43:11 +0300

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,1 +1,28 @@
+plugins {
+    id "com.android.application"
+    id "kotlin-android"
+    id "dev.flutter.flutter-gradle-plugin"
+}
 
+android {
+    namespace "com.example.wchat_alwzeer"
+    compileSdk 34
+
+    defaultConfig {
+        applicationId "com.example.wchat_alwzeer"
+        minSdk 21
+        targetSdk 34
+        versionCode 1
+        versionName "1.0.0"
+    }
+
+    buildTypes {
+        release {
+            signingConfig signingConfigs.debug
+        }
+    }
+}
+
+flutter {
+    source '../..'
+}
